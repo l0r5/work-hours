cf66b286ab6e797dfd576c8cf77a3527934b22f4
l0r5 <lars-braitinger@web.de>
2021-04-20 20:52:21 +0200

    Add first state handlers

--- src/components/workhours/WorkHoursInput/WorkHoursInput.js
+++ src/components/workhours/WorkHoursInput/WorkHoursInput.js
@@ -1,15 +1,33 @@
 import styles from './WorkHoursInput.module.css';
+import {useRef, useState} from 'react';
 
 const WorkHoursInput = (props) => {
+
+    const customerInputRef = useRef();
+    const [enteredCustomer, setEnteredCustomer] = useState('');
+
+    const customerInputChangeHandler = event => {
+        setEnteredCustomer(event.target.value);
+    };
+
+    const formSubmissionHandler = event => {
+        event.preventDefault();
+
+        const enteredValue = customerInputRef.current.value;
+        console.log(enteredValue);
+
+        setEnteredCustomer('');
+    };
+
     return (
-        <form>
+        <form onSubmit={formSubmissionHandler}>
             <div className={styles.formControl}>
                 <label htmlFor='date'>Date</label>
                 <input type='date' id='date'/>
             </div>
             <div className={styles.formControl}>
                 <label htmlFor='customer'>Customer</label>
-                <input type='text' id='customer'/>
+                <input ref={customerInputRef} type='text' id='customer' onChange={customerInputChangeHandler} value={enteredCustomer}/>
             </div>
             <div className={styles.formControl}>
                 <label htmlFor='text'>Location</label>
