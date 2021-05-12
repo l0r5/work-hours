fd3ce92a4edd8e8970e7629b1db0942fae790669
l0r5 <lars-braitinger@web.de>
2021-05-12 18:15:17 +0200

    Revert "Further refactor the table components"
    This reverts commit af4ee3a4

--- src/components/Tables/WorkHoursTable/WorkHoursTable.js
+++ src/components/Tables/WorkHoursTable/WorkHoursTable.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import WorkHoursTableRow from './WorkHoursTableRow/WorkHoursTableRow';
 import EnhancedTable from '../../UI/EnhancedTable/EnhancedTable';
+
 
 const headCells = [
     {id: 'view', numeric: false, label: ''},
@@ -17,7 +19,24 @@ const headCells = [
     {id: 'machineHours', numeric: true, label: 'Machine Hours'},
 ];
 
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+    }
+}));
+
 const WorkHoursTable = (props) => {
+
+    const materialClasses = useStyles();
+    // const table = useTable({rows: [...props.items]});
+
+    let rows = null;
+    if (props.items) {
+        rows = [...props.items];
+    }
+
 
     const createSpecificRow = (rowProps) => {
         return (
@@ -27,6 +46,8 @@ const WorkHoursTable = (props) => {
                 item={rowProps.row}
                 isSelected={rowProps.isSelected}
                 labelId={rowProps.labelId}
+
+                //TODO Refactor
                 onEditClick={props.onEditItemClick}
                 onDetailViewClick={props.onDetailViewClick}
             />);
@@ -37,8 +58,64 @@ const WorkHoursTable = (props) => {
             rows={props.items}
             headCells={headCells}
             createSpecificRow={createSpecificRow}
-            headerHasCheckBox={true}
         />
+        // <div className={classes.Root}>
+        //     <Paper className={materialClasses.paper}>
+        //         <TableToolbar numSelected={table.selected.length}/>
+        //         <TableContainer>
+        //             <Table
+        //                 className={classes.Table}
+        //                 aria-labelledby="tableTitle"
+        //                 size="medium"
+        //                 aria-label="Arbeitsstunden Tabelle"
+        //             >
+        //                 <EnhancedTableHead
+        //                     hasCheckbox={true}
+        //                     headCells={headCells}
+        //                     numSelected={table.selected.length}
+        //                     order={table.order}
+        //                     orderBy={table.orderBy}
+        //                     onSelectAllClick={table.onSelectAllCheckboxesHandler}
+        //                     onRequestSort={table.onSortRequestHandler}
+        //                     rowCount={rows ? rows.length : 0}
+        //                 />
+        //                 <TableBody>
+        //                     {table.tableSort.stableSort(rows, table.tableSort.getComparator(table.order, table.orderBy))
+        //                         .slice(table.page * table.rowsPerPage, table.page * table.rowsPerPage + table.rowsPerPage)
+        //                         .map((row, index) => {
+        //                             const isItemSelected = table.isSelected(row.id);
+        //                             const labelId = `enhanced-table-checkbox-${index}`;
+        //                             return (
+        //                                 <WorkHoursTableRow
+        //                                     key={row.id}
+        //                                     click={table.onCheckBoxClickHandler}
+        //                                     item={row}
+        //                                     isSelected={isItemSelected}
+        //                                     labelId={labelId}
+        //                                     onEditClick={props.onEditItemClick}
+        //                                     onDetailViewClick={props.onDetailViewClick}
+        //                                 />
+        //                             );
+        //                         })}
+        //                     {table.emptyRows > 0 && (
+        //                         <TableRow style={{height: 53 * table.emptyRows}}>
+        //                             <TableCell colSpan={6}/>
+        //                         </TableRow>
+        //                     )}
+        //                 </TableBody>
+        //             </Table>
+        //         </TableContainer>
+        //         <TablePagination
+        //             rowsPerPageOptions={[5, 10, 25]}
+        //             component="div"
+        //             count={rows ? rows.length : 0}
+        //             rowsPerPage={table.rowsPerPage}
+        //             page={table.page}
+        //             onChangePage={table.onPageChangeHandler}
+        //             onChangeRowsPerPage={table.onRowsPerPageChangeHandler}
+        //         />
+        //     </Paper>
+        // </div>
     );
 }
 
--- src/components/UI/EnhancedTable/EnhancedTable.js
+++ src/components/UI/EnhancedTable/EnhancedTable.js
@@ -10,7 +10,6 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
 import EnhancedTableHead from './EnhancedTableHead/EnhancedTableHead';
-import useTable from '../../../hooks/use-table';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -20,24 +19,91 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const EnhancedTable = (props) => {
-
     const materialClasses = useStyles();
-    const {
-        order,
-        orderBy,
-        selected,
-        page,
-        rowsPerPage,
-        onSortRequestHandler,
-        onSelectAllCheckboxesHandler,
-        onCheckBoxClickHandler,
-        onPageChangeHandler,
-        onRowsPerPageChangeHandler,
-        getComparator,
-        stableSort,
-        isSelected,
-        emptyRows
-    } = useTable({rows: [...props.rows]});
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('data');
+    const [selected, setSelected] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    let rows = [];
+    if (props.rows) {
+        rows = [...props.rows];
+    }
+
+    const onSortRequestHandler = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const onSelectAllCheckboxesHandler = (event) => {
+        if (event.target.checked) {
+            const newSelected = rows.map((n) => n.id);
+            setSelected(newSelected);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const onCheckBoxClickHandler = (event, id) => {
+        const selectedIndex = selected.indexOf(id);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, id);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1),
+            );
+        }
+        setSelected(newSelected);
+    };
+
+    const onPageChangeHandler = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const onRowsPerPageChangeHandler = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const descendingComparator = (a, b, orderBy) => {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    };
+
+    const getComparator = (order, orderBy) => {
+        return order === 'desc'
+            ? (a, b) => descendingComparator(a, b, orderBy)
+            : (a, b) => -descendingComparator(a, b, orderBy);
+    };
+
+    const stableSort = (array, comparator) => {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        return stabilizedThis.map((el) => el[0]);
+    }
+
+    const isSelected = (id) => selected.indexOf(id) !== -1;
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows ? rows.length : 0 - page * rowsPerPage);
+
 
     return (
         <div className={classes.Root}>
@@ -51,17 +117,17 @@ const EnhancedTable = (props) => {
                         aria-label="Arbeitsstunden Tabelle"
                     >
                         <EnhancedTableHead
-                            hasCheckbox={props.headerHasCheckBox}
+                            hasCheckbox={true}
                             headCells={props.headCells}
                             numSelected={selected.length}
                             order={order}
                             orderBy={orderBy}
                             onSelectAllClick={onSelectAllCheckboxesHandler}
                             onRequestSort={onSortRequestHandler}
-                            rowCount={props.rows ? props.rows.length : 0}
+                            rowCount={rows ? rows.length : 0}
                         />
                         <TableBody>
-                            {stableSort(props.rows, getComparator(order, orderBy))
+                            {stableSort(rows, getComparator(order, orderBy))
                                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                 .map((row, index) => {
                                     const labelId = `enhanced-table-checkbox-${index}`;
@@ -86,7 +152,7 @@ const EnhancedTable = (props) => {
                 <TablePagination
                     rowsPerPageOptions={[5, 10, 25]}
                     component="div"
-                    count={props.rows ? props.rows.length : 0}
+                    count={rows ? rows.length : 0}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     onChangePage={onPageChangeHandler}
@@ -95,6 +161,7 @@ const EnhancedTable = (props) => {
             </Paper>
         </div>
     );
+
 };
 
 export default EnhancedTable;
